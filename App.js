00d6a1c831910d14d508bb85565edd5605d581cf
Yashs1623 <yashs230602@gmail.com>
2021-12-18 19:31:02 +0530

    bugs fixed

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import TopTabNavigation from './src/screens/TopTabNavigation/TopTabsNavigation';
 import { createStackNavigator } from '@react-navigation/stack';
 import AppliancesDetails from './src/screens/AppliancesDetails';
-AppliancesDetails
+import Login from './src/screens/Login';
+import Splash from './src/screens/Splash';
+import Register from './src/screens/Register';
 
 const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="TopTabNavigation">
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name='Login' component={Login} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name='Register' component={Register} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name='Splash' component={Splash} options={{ headerShown: false }}></Stack.Screen>
         <Stack.Screen name='TopTabNavigation' component={TopTabNavigation} options={{ headerShown: false }}></Stack.Screen>
         <Stack.Screen name='AppliancesDetails' component={AppliancesDetails} options={{ headerShown: false }}></Stack.Screen>
       </Stack.Navigator>
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import LottieView from 'lottie-react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const Splash = () => (
-  <LinearGradient
-    colors={['#1A1B2F', '#162447', '#1F4068']}
-    style={styles.linearGradient}>
-    <LottieView
-      source={require('../assets/quote.json')}
-      autoPlay={true}
-      loop
-      style={styles.lottieview}
-    />
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Login')
+    }, 2200);
+  }, []);
+  return (
+    <LinearGradient
+      colors={['#1A1B2F', '#162447', '#1F4068']}
+      style={styles.linearGradient}>
+      <LottieView
+        source={require('../assets/quote.json')}
+        autoPlay={true}
+        loop
+        style={styles.lottieview}
+      />
 
-    <Text style={styles.header}>QuoteIT</Text>
-  </LinearGradient>
-);
+      <Text style={styles.header}>QuoteIT</Text>
+    </LinearGradient>
+
+
+  );
+}
+
 export default Splash;
+
 const styles = StyleSheet.create({
   linearGradient: {
     flex: 1,
@@ -37,7 +49,7 @@ const styles = StyleSheet.create({
   lottieview: {
     height: 250,
     width: 250,
-    marginTop: 40,
+    marginTop: hp('10%'),
     alignSelf: 'center',
   },
   header: {
@@ -45,6 +57,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
     fontSize: 50,
-    marginTop: 40,
+    marginTop: 100,
   },
 });
