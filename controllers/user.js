94ec417825f83b3e24239355e37a02040f6b98ed
lokita282 <lokitavarma282@gmail.com>
2021-12-21 10:32:47 +0530

    improved user dashboard and updated link in whatsapp message

--- controllers/user.js
+++ controllers/user.js
@@ -1,121 +1,126 @@
-import User from '../models/User.js';
-import Quotation from '../models/Quotation.js';
-import Query from '../models/Query.js';
-import { sendWelcomeEmail } from '../emails/account.js';
+import User from '../models/User.js'
+import Quotation from '../models/Quotation.js'
+import Query from '../models/Query.js'
+import { sendWelcomeEmail } from '../emails/account.js'
 
 //Register a user
 const registerNewUser = async (req, res) => {
   try {
-    const user = new User(req.body);
-    const token = await user.generateAuthToken();
-    await user.save();
+    const user = new User(req.body)
+    const token = await user.generateAuthToken()
+    await user.save()
     console.log(user)
-    sendWelcomeEmail(user.email, user.name, user.role);
-    res.status(201).send(token);
+    sendWelcomeEmail(user.email, user.name, user.role)
+    res.status(201).send(token)
   } catch (e) {
     res.status(400).json({
       success: false,
       message: e.message,
-    });
+    })
   }
-};
+}
 
 //Login user
 const loginUser = async (req, res) => {
   try {
-    const user = await User.findByCredentials(
-      req.body.email,
-      req.body.password
-    );
-    const token = await user.generateAuthToken();
-    res.send(token);
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.send(token)
   } catch (e) {
     res.status(400).json({
       success: false,
       message: e.message,
-    });
+    })
   }
-};
+}
 
 //Logout User
 const logoutUser = async (req, res) => {
   try {
-    req.tokens === '';
+    req.tokens === ''
     // await req.data.save()
 
     res.json({
       success: true,
       message: 'Logged out successfully',
-    });
+    })
   } catch (e) {
-    console.log(e);
+    console.log(e)
     res.status(500).json({
       success: false,
       message: e.message,
-    });
+    })
   }
-};
+}
 
 //Get all users
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find({});
-    res.json(users);
+    const users = await User.find({})
+    res.json(users)
   } catch (e) {
     res.json({
       success: false,
       message: e.message,
-    });
+    })
   }
-};
+}
 
 //Get Personal Profile
 const getProfile = async (req, res) => {
   try {
-    const user = await req.user;
+    const user = await req.user
     res.json({
       success: true,
       data: user,
-    });
+    })
   } catch (e) {
     res.status(500).json({
       success: false,
       message: e.message,
-    });
+    })
   }
-};
+}
 
 //Update user details
 const updateUser = async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'email', 'password', 'contact', 'college', 'branch', 'batch'];
+  const updates = Object.keys(req.body)
+  const allowedUpdates = [
+    'name',
+    'email',
+    'password',
+    'contact',
+    'college',
+    'branch',
+    'batch',
+  ]
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
-  );
+  )
 
   if (!isValidOperation) {
-    return res.status(400).send({ error: 'invalid Updates' });
+    return res.status(400).send({ error: 'invalid Updates' })
   }
   if (!req.user) {
     return res.status(401).json({
       success: false,
       message: 'Please login',
-    });
+    })
   }
 
   try {
-    updates.forEach((update) => (req.user[update] = req.body[update]));
+    updates.forEach((update) => (req.user[update] = req.body[update]))
 
-    req.user.save();
+    req.user.save()
 
-    res.send(req.user);
+    res.send(req.user)
   } catch (e) {
     res.status(400).json({
       success: false,
       message: e.message,
-    });
+    })
   }
-};
+}
 
 //Delete User
 const deleteUser = async (req, res) => {
@@ -123,37 +128,43 @@ const deleteUser = async (req, res) => {
     return res.status(401).json({
       success: false,
       message: 'Please login',
-    });
+    })
   }
   try {
-    await req.user.remove();
+    await req.user.remove()
     res.json({
       success: true,
       data: 'User deleted successfully',
-    });
+    })
   } catch (e) {
     res.status(500).json({
       success: false,
       message: e.message,
-    });
+    })
   }
-};
+}
 
 //Get Client Dashboard
 
 const getClientDashboard = async (req, res) => {
   try {
-    const quotations = await Quotation.find({quotedToEmail: req.user.email})
-    console.log(quotations);
+    const quotations = await Quotation.find({ quotedToEmail: req.user.email })
+    if (!quotations) {
+      return res.json({
+        success: true,
+        Error: 'There are no quotations to show!',
+      })
+    }
+    console.log(quotations)
     res.json({
       success: true,
       data: quotations,
-    });
+    })
   } catch (e) {
     res.status(500).json({
       success: false,
       message: e.message,
-    });
+    })
   }
 }
 
@@ -162,10 +173,10 @@ const getClientDashboard = async (req, res) => {
 const getVendorDashboard = async (req, res) => {
   try {
     const queries = await Query.find({})
-    console.log(queries[0].queriedTo[0].email);
+    console.log(queries[0].queriedTo[0].email)
     const queriesToDisplay = []
-    console.log(queries.length);
-    // console.log(queries[0].queriedTo[0]); 
+    console.log(queries.length)
+    // console.log(queries[0].queriedTo[0]);
     // console.log(queries[0])
     // console.log(Object.values(queries.queriedTo))
     // queries.forEach(query, index => {
@@ -175,19 +186,18 @@ const getVendorDashboard = async (req, res) => {
     //    }
     //   //  console.log(queriesToDisplay);
     // });
-    for(let i = 0; i<queries.length; i++)
-    res.json({
-      success: true,
-      data: queriesToDisplay,
-    });
+    for (let i = 0; i < queries.length; i++)
+      res.json({
+        success: true,
+        data: queriesToDisplay,
+      })
   } catch (e) {
     res.status(500).json({
       success: false,
       message: e.message,
-    });
+    })
   }
 }
-
 
 export {
   registerNewUser,
@@ -198,5 +208,5 @@ export {
   updateUser,
   deleteUser,
   getClientDashboard,
-  getVendorDashboard
-};
+  getVendorDashboard,
+}
